5a739ed6cd4b94bd7d042538957f2f203c9470ed
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2022-10-19 15:47:54 +0200

    Pushing chaos code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-const slideshow = document.getElementsByClassName('slideshow');
-const slideshowFigure = document.getElementsByClassName('slideshow__figure')
+const slides = document.getElementsByClassName('slideshow__figure')
 
 const leftArrow = document.getElementById('button-left')
 const rightArrow = document.getElementById('button-right')
+
+const dots = document.getElementsByClassName('slideshow__dot')
+
+
+//event listeners;
+dots.addEventListener('click', );
+leftArrow.addEventListener('click', handlePreviousSlide);
+
+
+let slideIndex = 1;
+
+function handlePreviousSlide() {
+	if (number > slides.length) {slideIndex = 1};
+	if (number < 1) {slideIndex = slides.length};
+	for (index = 0; index < slides.length; index += 1) {
+		slides[index].className = slides[index].className.replace('.slideshow__figure--visible', '.slideshow__figure')
+	};
+	for (index = 0; index < dots.length; index += 1) {
+		dots[index].className = dots[index].className.replace(' active', '')
+	};
+;}
 
