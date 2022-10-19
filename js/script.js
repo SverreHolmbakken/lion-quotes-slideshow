2ce70ffa7ddb0ec5188f6ce34d81b983733495a3
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2022-10-19 15:14:50 +0200

    Got elements from html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-const slideshow = document.querySelector('.slideshow');
-const slideshowFigure = document.querySelectorAll('.slideshow__figure')
+const slideshow = document.getElementsByClassName('slideshow');
+const slideshowFigure = document.getElementsByClassName('slideshow__figure')
+
+const leftArrow = document.getElementById('button-left')
+const rightArrow = document.getElementById('button-right')
 
